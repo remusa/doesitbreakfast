5aaf72079bafb8809dcb881d1e946202258202e1
R Sánchez <rene.as.himself@gmail.com>
2020-01-21 01:10:54 -0600

    Footer fixed to bottom of the screen

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,15 +11,18 @@ interface Props {
 const Layout: React.FC<Props> = props => {
   return (
     <Flex w='100%' h='100vh' flexDirection='column' alignItems='center'>
+      {/* <Flex w='100%' h='100vh' flexDirection='column' alignItems='center'> */}
       <Header />
 
       <Flex
         as='main'
-        h='100vh'
-        paddingY='1.5rem'
         flexDirection='column'
+        flexGrow={1}
+        flexShrink={0}
+        flexBasis='auto'
         justifyContent='flex-start'
         alignItems='center'
+        paddingY='1.5rem'
       >
         {props.children}
       </Flex>
