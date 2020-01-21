6bf5ad2dcc25cdb234a20f1bca65bbcd20eaa8dd
R Sánchez <rene.as.himself@gmail.com>
2020-01-21 00:17:21 -0600

    File upload

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -32,18 +32,42 @@ const Submit: NextPage<Props> = () => {
   const toast = useToast()
   const { user, loggedIn } = useAuth()
   const router = useRouter()
+  // const fileInput = useRef(null)
 
   if ((!user || !loggedIn) && typeof window !== 'undefined') {
     router.push('/')
   }
 
   const onSubmit = async (data, e) => {
+    // let image = null
+    // let imageURL = null
+    // if (fileInput.current.files) {
+    //   image = fileInput.current.files[0]
+    //   console.log('file', image.name)
+    // }
+    // if (image) {
+    //   await storage
+    //     .ref()
+    //     .child('entries')
+    //     .child(data.id)
+    //     .child(image.name)
+    //     .put(image)
+    //     .then(res => res.ref.getDownloadURL())
+    //     .then(url => {
+    //       imageURL = url
+    //     })
+    //     .catch(e => console.log(`Error uploading file: ${e.message}`))
+    // }
+    // console.log('imageURL', imageURL)
+
     const newEntry = {
       ...data,
       breaksFast: data.breaksFast === 'yes' ? true : false,
       sources: data.sources.split('\n'),
       approved: false,
     }
+
+    // if (imageURL) newEntry.imageURL = imageURL
 
     const res = await firestore
       .collection('entries')
@@ -72,7 +96,7 @@ const Submit: NextPage<Props> = () => {
         duration: 5000,
         isClosable: true,
       })
-      e.target.reset()
+      // e.target.reset()
     }
   }
 
@@ -100,17 +124,28 @@ const Submit: NextPage<Props> = () => {
               <FormLabel htmlFor='type'>Type</FormLabel>
               <Select ref={register} name='type' variant='flushed'>
                 <option value='Common Drinks'>Common Drinks</option>
-                <option value='Additions/Condiments'>Additions/Condiments</option>
-                <option value='Non-caloric Sweeteners'>Non-caloric Sweeteners</option>
+                <option value='Additions/Condiments'>
+                  Additions/Condiments
+                </option>
+                <option value='Non-caloric Sweeteners'>
+                  Non-caloric Sweeteners
+                </option>
                 <option value='Supplements'>Supplements</option>
-                <option value='Breath-Freshening Items'>Breath-Freshening Items</option>
+                <option value='Breath-Freshening Items'>
+                  Breath-Freshening Items
+                </option>
               </Select>
               {errors.type && <ErrorMessage message={errors.type.message} />}
             </FormControl>
 
             <FormControl mb={4}>
               <FormLabel htmlFor='breaksFast'>Breaks fast?</FormLabel>
-              <RadioGroup name='breaksFast' spacing={5} isInline defaultValue='yes'>
+              <RadioGroup
+                name='breaksFast'
+                spacing={5}
+                isInline
+                defaultValue='yes'
+              >
                 <Radio
                   name='breaksFast'
                   value='yes'
@@ -128,19 +163,35 @@ const Submit: NextPage<Props> = () => {
                   No
                 </Radio>
               </RadioGroup>
-              {errors.breaksFast && <ErrorMessage message={errors.breaksFast.message} />}
+              {errors.breaksFast && (
+                <ErrorMessage message={errors.breaksFast.message} />
+              )}
             </FormControl>
 
             <FormControl>
               <FormLabel htmlFor='description'>Description</FormLabel>
-              <Input ref={register({ required: true })} name='description' variant='flushed' />
-              {errors.description && <ErrorMessage message={errors.description.message} />}
+              <Input
+                ref={register({ required: true })}
+                name='description'
+                variant='flushed'
+              />
+              {errors.description && (
+                <ErrorMessage message={errors.description.message} />
+              )}
             </FormControl>
 
             <FormControl>
               <FormLabel htmlFor='sources'>Sources</FormLabel>
               <Textarea ref={register} name='sources' variant='flushed' />
-              {errors.sources && <ErrorMessage message={errors.sources.message} />}
+              {errors.sources && (
+                <ErrorMessage message={errors.sources.message} />
+              )}
+            </FormControl>
+
+            <FormControl>
+              <FormLabel htmlFor='image'>Image URL</FormLabel>
+              <Input ref={register} name='image' variant='flushed' />
+              {errors.image && <ErrorMessage message={errors.image.message} />}
             </FormControl>
 
             <Flex justifyContent='center' mt={4}>
