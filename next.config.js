414acecdf940959eb54f69a038e6b555af79b479
R Sánchez <rene.as.himself@gmail.com>
2020-01-20 19:39:47 -0600

    Update next.config.js
    * Environment variables for Firebase

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 // const Firebase = require('./lib/firebase')
+// import {firestore} from './lib/firebase'
 // const firestore = Firebase.firestore
 // const fetch = require('isomorphic-unfetch');
 const withOffline = require('next-offline')
-// import {firestore} from './lib/firebase'
 const env = require('./.env')
-// require('dotenv').config()
+
+require('dotenv').config()
 
 const nextConfig = {
+  // env: env,
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+  },
   // exportTrailingSlash: true,
   // exportPathMap: async function() {
   //   const paths = {
@@ -20,7 +31,6 @@ const nextConfig = {
   //   })
   //   return paths
   // },
-  env: env,
   target: 'serverless',
   workboxOpts: {
     swDest: 'public/service-worker.js',
