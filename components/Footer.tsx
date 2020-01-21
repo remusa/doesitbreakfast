e91ab237c96916f54db74194d7a4fa364fafd1b9
R Sánchez <rene.as.himself@gmail.com>
2020-01-20 21:50:32 -0600

    Fixed Footer spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/core'
+import { Flex, Text } from '@chakra-ui/core'
 
 interface Props {}
 
@@ -13,8 +13,22 @@ const Footer: React.FC<Props> = () => {
       justifyContent='space-between'
       align-items='center'
     >
-      <p>Copyright © RMS 2020.</p>
-      {/* <Toggle isOn={theme === 'dark'} handleToggle={toggleTheme} /> */}
+      <Flex w={150} justify='space-between'>
+        <Text>Built with </Text>
+        <Text color='error'> ❤ </Text>
+        <Text>by </Text>
+        <Text>
+          <a
+            href='https://renems.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            RMS
+          </a>
+        </Text>
+        {/* <p>Copyright © RMS 2020.</p> */}
+        {/* <Toggle isOn={theme === 'dark'} handleToggle={toggleTheme} /> */}
+      </Flex>
     </Flex>
   )
 }
