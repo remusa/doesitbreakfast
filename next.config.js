ab7be573b7571dcd0eacae0018f41d287d285afd
R Sánchez <rene.as.himself@gmail.com>
2020-01-23 19:38:14 -0600

    Moved env variables

--- next.config.js
+++ next.config.js
@@ -13,7 +13,6 @@ const withPlugins = require('next-compose-plugins')
 const bundleAnalyzer = require('@next/bundle-analyzer')
 
 const nextConfig = {
-  // env: require('./.env'),
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
