1f0f3b79812ef93b12c281a985bad87219ec1b00
R Sánchez <rene.as.himself@gmail.com>
2020-01-23 19:23:11 -0600

    Moved Firebase config to environment variables
    * Changed Next.js to load them using dotenv

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,7 +2,17 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
-import firebaseConfig from './config.json'
+// import firebaseConfig from './config.json'
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
 
 const firebaseApp = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
