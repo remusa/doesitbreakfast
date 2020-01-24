40566f87db676a1d9b2ec95f7a4e2c0ef9337fae
R Sánchez <rene.as.himself@gmail.com>
2020-01-23 18:58:26 -0600

    Firebase functions
    * Set Node to version 10
    * Example cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,3 +21,65 @@ export const getAllPosts = functions.https.onRequest(async (req, res) => {
 
   res.json({ posts })
 })
+
+export const sanitizeContent = functions.firestore
+  .document('posts/{postId}')
+  .onWrite(async change => {
+    if (!change.after.exists) return
+
+    // @ts-ignore
+    const { content, sanitized } = change.after.data()
+
+    if (content && !sanitized) {
+      return change.after.ref.update({
+        content: content.replace(/CoffeeScript/g, '***********'),
+        sanitized: true,
+      })
+    }
+
+    return null
+  })
+
+export const incrementCommentCount = functions.firestore
+  .document('posts/{postId}/comments/{commentId}')
+  .onCreate(async (snapshot, context) => {
+    const { postId } = context.params
+    const postRef = firestore.doc(`posts/${postId}`)
+
+    const snap = await postRef.get()
+    const comments = snap.get('comments')
+
+    return postRef.update({ comments: comments + 1 })
+  })
+
+export const decrementCommentCount = functions.firestore
+  .document('posts/{postId}/comments/{commentId}')
+  .onCreate(async (snapshot, context) => {
+    const { postId } = context.params
+    const postRef = firestore.doc(`posts/${postId}`)
+
+    const snap = await postRef.get()
+    const comments = snap.get('comments')
+
+    return postRef.update({ comments: comments - 1 })
+  })
+
+export const updateUserInformation = functions.firestore
+  .document('users/{userId}')
+  .onUpdate(async (snapshot, context) => {
+    // @ts-ignore
+    const { displayName } = snapshot.data()
+
+    const postsRef = firestore
+      .collection('posts')
+      // @ts-ignore
+      .where('user.uid', '==', snapshot.id)
+
+    // @ts-ignore
+    return postsRef.get(postSnaps => {
+      // @ts-ignore
+      postSnaps.forEach(doc => {
+        doc.ref.update({ 'user.displayName': displayName })
+      })
+    })
+  })
