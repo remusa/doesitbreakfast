cfc4444e679d7ba571b8c297fc4a9db194e2be92
R Sánchez <rene.as.himself@gmail.com>
2020-01-23 18:29:24 -0600

    Added example Firebase cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,23 @@
-import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
+
+admin.initializeApp(functions.config().firebase)
+
+const firestore = admin.firestore()
+firestore.settings({
+  timestampsInSnapshots: true,
+})
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!')
+})
+
+export const getAllPosts = functions.https.onRequest(async (req, res) => {
+  const snapshot = await firestore.collection('posts').get()
+  const posts = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+
+  res.json({ posts })
+})
